db83f33da24638daf9b0f454481e964bda87c542
mike.soft.dev <mike.soft.dev@gmail.com>
2018-07-13 02:44:11 -0600

    prototype

---
+++ AlgoApp/Node.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoApp
+{
+    class Node
+    {
+
+        private int key;
+        private List<string> data;
+        private Node left;
+        private Node right;
+
+
+
+        public Node(int key, List<string> items) {
+            this.key = key;
+            this.data = items;
+            this.left = null;
+            this.right = null;
+        }
+
+        public Node GetLeft() {
+            return this.left;
+        }
+
+        public Node GetRight() {
+            return this.right;
+        }
+
+        public int GetKey()
+        {
+            return this.key;
+        }
+
+        public void Add(int key, string item) {
+            if (this.key == key) {
+                this.data.Add(item);
+            }
+            else if (key < this.key)
+            {
+                this.AddLeft(key, item);
+            }
+            else {
+                this.AddRight(key, item);
+            }
+
+        }
+
+        public void AddLeft(int key, string item) {
+
+            if (this.left == null)
+            {
+                List<string> new_list = new List<string>();
+                new_list.Add(item);
+                Node new_item = new Node(key, new_list);
+                this.left = new_item;
+            }
+            else {
+                this.left.Add(key, item);
+            }
+
+        }
+
+        public void AddRight(int key, string item) {
+
+            if (this.right == null)
+            {
+                List<string> new_list = new List<string>();
+                new_list.Add(item);
+                Node new_item = new Node(key, new_list);
+                this.right = new_item;
+            }
+            else
+            {
+                this.right.Add( key, item);
+            }
+
+        }
+
+        public Node Search(int key) {
+
+            return null;
+        }
+
+        public string Print() {
+
+            string res = "Node " + this.key;
+            if (this.left != null)
+            {
+                res = res + " < (L) " + this.left.Print();
+            }
+            else {
+                res = res + " < ";
+            }
+
+            if (this.right != null)
+            {
+                res = res + " (R) " + this.right.Print() + " >";
+            }
+            else {
+                res = res + " > ";
+            }
+
+            return res;
+        }
+
+        public List<string> GetData() {
+            return this.data;
+        }
+
+    }
+}
---
+++ AlgoApp/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> l1 = new List<string>();
+            l1.Add("hello");
+            Node test = new Node(12, l1);
+            test.Add(10, "hey");
+            test.Add(20, "yo");
+            test.Add(2, "yea");
+            test.Add(19, "ok");
+            test.Add(5, "test");
+
+
+            Console.WriteLine(test.Print());
+
+
+            Console.WriteLine("Hello World!");
+            string user_char;
+            char ch;
+
+            // Keep the console window open in debug mode.
+            string[] lines = { "First line", "Second line", "Third line" };
+
+            foreach (string line in lines) {
+
+                Console.WriteLine(line);
+                Console.WriteLine("Write a line:");
+
+                user_char = Console.ReadLine();
+                Console.WriteLine("You pressed '{0}'", user_char);
+            }
+
+        }
+    }
+}
